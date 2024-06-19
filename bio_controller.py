de77b1326eacb4c1c74726a172eef060ee9f6dcd
Joel Currie <joelcurrie3@gmail.com>
2024-06-19 17:48:11 +0100

    Add files via upload

--- bio_controller.py
+++ bio_controller.py
@@ -1,9 +1,13 @@
 import math
-l = 0.053
-r = 0.041/2
+import numpy as np
+import matplotlib.pyplot as plt
 
-v_x = 10
-theta_dot = math.pi * 0
+l = 5.3
+r = 4.1/2
+
+v_x = 1
+theta_dot = 1 * math.pi
+
 def InverseKinematics(v_x, theta_dot, l, r):
     # Inverse Kinematics for differential drive robot
     phi_dot_L = (v_x/r) - (l * theta_dot)/(2*r)
@@ -11,13 +15,27 @@ def InverseKinematics(v_x, theta_dot, l, r):
     return phi_dot_L, phi_dot_R
 
 
+def ttpl_velocity(phi_dot_L, phi_dot_R):
+    if phi_dot_L == -phi_dot_R:
+        k = 0.01
+    elif phi_dot_L == phi_dot_R:
+        k = 1
+    else:
+        k_scale = 1/3.8
+        R = (l/2) * (phi_dot_L + phi_dot_R)/(phi_dot_R - phi_dot_L)
+        k = k_scale * (1/R) ** -(1/3)
+    
+    phi_dot_LB = k * phi_dot_L
+    phi_dot_RB = k * phi_dot_R
+
+    return phi_dot_LB, phi_dot_RB
 
 phi_dot_L, phi_dot_R = InverseKinematics(v_x, theta_dot, l, r)
-if not phi_dot_L == phi_dot_R:
-    R = (l/2) * (phi_dot_L + phi_dot_R)/(phi_dot_R - phi_dot_L)
-    k = (1/R) ** -(1/3)
-else R = 
+phi_dot_LB, phi_dot_RB = ttpl_velocity(phi_dot_L, phi_dot_R)
 
-phi_dot_LB = k * phi_dot_L
-phi_dot_RB = k * phi_dot_R
-print(phi_dot_RB)
+
+#plt.plot(theta_dot, K)
+#plt.title("Biological Controller Response")
+#plt.xlabel("Angular Velocity (ω) [rad/s]")
+#plt.ylabel("Controller Gain (k)")
+#plt.show()
